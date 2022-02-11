33321274738849c67efcac9580dc9f172dbf1263
derev <dereviannykh.v@gmail.com>
2022-02-11 16:26:24 +0300

    123

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ static = os.path.join('static')
 #     return "Миссия Колонизация Марса"
 
 
-@app.route('/')
-@app.route('/index')
-def image_mars():
-    return render_template('base.html')
+@app.route('/<title>')
+@app.route('/index/<title>')
+def image_mars(title: str):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
