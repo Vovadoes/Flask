0950ae59f21fd273f7bc32008ee682395de3ac35
derev <dereviannykh.v@gmail.com>
2022-02-28 18:32:29 +0300

    Первая работа

--- data/users.py
+++ data/users.py
@@ -22,3 +22,6 @@ class User(SqlAlchemyBase):
     # modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
     #                                   default=datetime.datetime.now)
     # news = orm.relation("News", back_populates='user')
+
+    def __repr__(self):
+        return f'<Colonist> {self.id} {self.surname} {self.name}'
--- main.py
+++ main.py
@@ -1,77 +1,14 @@
 from flask import Flask
-from data import db_session
-from data.Jobs import Jobs
-from data.users import User
+
+# from data import db_session
+# from data.Jobs import Jobs
+# from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+global_init("db/blogs.db")
+db_sess = create_session()
 
-def main():
-    db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-
-    user1 = User()
-    user1.name = 'Ridley'
-    user1.surname = 'Scott'
-    user1.age = 21
-    user1.position = 'captain'
-    user1.speciality = 'research engineer'
-    user1.address = 'module_1'
-    user1.email = 'scott_chief@mars.org'
-    db_sess.add(user1)
-
-    user2 = User()
-    user2.name = 'vova'
-    user2.surname = 'Scott'
-    user2.age = 15
-    user2.position = 'user'
-    user2.speciality = 'research engineer'
-    user2.address = 'module_2'
-    user2.email = 'vova@mars.org'
-    db_sess.add(user2)
-
-    user3 = User()
-    user3.name = 'danila'
-    user3.surname = 'Scott'
-    user3.age = 15
-    user3.position = 'user'
-    user3.speciality = 'research engineer'
-    user3.address = 'module_3'
-    user3.email = 'danila@mars.org'
-    db_sess.add(user3)
-
-    user4 = User()
-    user4.name = 'dima'
-    user4.surname = 'Scott'
-    user4.age = 15
-    user4.position = 'user'
-    user4.speciality = 'research engineer'
-    user4.address = 'module_4'
-    user4.email = 'dima@mars.org'
-    db_sess.add(user4)
-
-    job = Jobs()
-    job.job = 'deployment of residential modules 1 and 2'
-    job.work_size = 15
-    job.is_finished = False
-    db_sess.add(job)
-
-    db_sess.commit()
-
-    job: Jobs = db_sess.query(Jobs).all()[0]
-
-    captain: User = db_sess.query(User).filter(User.position == 'captain')[0]
-
-    job.team_leader = captain.id
-
-    for user in db_sess.query(User).all():
-        user.id_job = job.id
-
-    db_sess.commit()
-
-    # app.run()
-
-
-if __name__ == '__main__':
-    main()
+for user in db_sess.query(User).all():
+    print(user.__repr__())
