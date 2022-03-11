c901fc4b011a148e6d2e224c562c0e37dd9cfc6b
student <dereviannykh.v@gmail.com>
2022-03-11 17:10:44 +0300

    upload

--- data/users.py
+++ data/users.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
-# from sqlalchemy import orm
+from sqlalchemy import orm
 
 
 class User(SqlAlchemyBase, UserMixin):
@@ -25,7 +25,7 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-    # news = orm.relation("News", back_populates='user')
+    news = orm.relation("News", back_populates='user')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
