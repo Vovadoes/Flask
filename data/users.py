5fddc1faa40c6cef4478ae01390fdd06ef1d5740
Vovadoes <62838512+Vovadoes@users.noreply.github.com>
2022-03-11 15:27:48 +0300

    Модель формы авторизации пользователя

--- data/users.py
+++ data/users.py
@@ -1,10 +1,15 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
+
+
 # from sqlalchemy import orm
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -17,11 +22,13 @@ class User(SqlAlchemyBase):
     address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
-    id_job = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("jobs.id"), nullable=True)
-    # hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    # modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
-    #                                   default=datetime.datetime.now)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
     # news = orm.relation("News", back_populates='user')
 
-    def __repr__(self):
-        return f'<Colonist> {self.id} {self.surname} {self.name}'
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,29 +1,105 @@
-from flask import Flask
-
-# from data import db_session
-# from data.Jobs import Jobs
-# from data.users import User
-from data.db_session import global_init, create_session
-from data.users import User
 import datetime
 
-from flask import Flask, render_template, redirect
+from flask import Flask, request, make_response, session, render_template
+from flask_login import LoginManager, login_user, login_required, logout_user
+from werkzeug.utils import redirect
+
 from data import db_session
+from data.db_session import global_init
 from data.users import User
-from data.Jobs import Jobs
+from forms.RegisterForm import RegisterForm
+from forms.UserForm import LoginForm
 
 app = Flask(__name__)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route("/")
-def Journal_of_works():
+
+@login_manager.user_loader
+def load_user(user_id):
     db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
-    return render_template("Journal_of_works.html", jobs=jobs)
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.email = form.email.data
+        user.name = form.name.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/')
+def main():
+    return render_template("main.html")
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    global_init("db/db.db")
+    app.run(port=8080, host='127.0.0.1')
