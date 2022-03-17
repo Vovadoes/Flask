4348d8a472438102c355bc47964090ffd082ee17
Vovadoes <62838512+Vovadoes@users.noreply.github.com>
2022-03-17 19:04:38 +0300

    Получение одной работы

--- data/Job_api.py
+++ data/Job_api.py
@@ -24,3 +24,25 @@ def get_jobs():
                     for item in news]
         }
     )
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_job(job_id: int):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Job).filter(Job.id == job_id).first()
+
+    if job is None:
+        return jsonify(
+            {
+                'job': None
+            }
+        )
+    else:
+        return jsonify(
+            {
+                'job':
+                    job.to_dict(only=(
+                        'id', 'team_leader', 'job', 'work_size', 'start_date', 'collaborators',
+                        'is_finished'))
+            }
+        )
